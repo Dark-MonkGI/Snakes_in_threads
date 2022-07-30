c6c62982ab03b87773e20f8bde696a0137779c77
Dark-MonkGI <gryaznov774@gmail.com>
2022-07-31 00:21:06 +0400

    a new class snake

---
+++ SnakesInThreads/Snake.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakesInThreads
+{
+    public struct Сoordinates
+    {
+        public int x;
+        public int y;
+        public Сoordinates(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    internal class Snake
+    {
+        public static readonly Сoordinates size = new Сoordinates(70, 24);
+        public static readonly char aSpace = ' ';
+        public static readonly char aWall = '#';
+        public static readonly char aSnakeBody = '*';
+        public static readonly char aEat = '$';
+
+    }
+}
